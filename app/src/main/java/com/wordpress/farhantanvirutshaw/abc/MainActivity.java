b659621ebfd760ee6dd728595dcc712bcf2250bc
Utshaw <farhan.tanvir.utshaw@gmail.com>
2017-05-12 01:57:43 +0600

    Final Release

--- app/src/main/java/com/wordpress/farhantanvirutshaw/abc/MainActivity.java
+++ app/src/main/java/com/wordpress/farhantanvirutshaw/abc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wordpress.farhantanvirutshaw.abc;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,71 +16,115 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_2);
 
-        final TextView numbers  = (TextView) findViewById(R.id.numbers);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
-        numbers.setOnClickListener(new View.OnClickListener() {
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
-                startActivity(numbersIntent);
+            public Fragment getItem(int position) {
+                switch (position)
+                {
+                    case 0:
+                        return new NumbersFragment();
+                    case 1:
+                        return new FamilyFragment();
+                    case 2:
+                        return new ColorsFragment();
+                    default:
+                        return new PhrasesFragment();
+                }
             }
 
-        });
-
-
-
-        TextView family = (TextView) findViewById(R.id.family);
-        family.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent familyIntent = new Intent(MainActivity.this,FamilyActivity.class);
-                startActivity(familyIntent);
+            public int getCount() {
+                return 4;
             }
-        });
 
-        // Find the View that shows the colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
-
-        // Set a click listener on that View
-        colors.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the colors category is clicked on.
             @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link ColorsActivity}
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-
-                // Start the new activity
-                startActivity(colorsIntent);
+            public CharSequence getPageTitle(int position) {
+                switch (position)
+                {
+                    case 0:
+                        return "Numbers";
+                    case 1:
+                        return "Family";
+                    case 2:
+                        return "Colors";
+                    default:
+                        return "Phrases";
+                }
             }
-        });
+        };
 
-        // Find the View that shows the phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-
-        // Set a click listener on that View
-        phrases.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the phrases category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link PhrasesActivity}
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-
-                // Start the new activity
-                startActivity(phrasesIntent);
-            }
-        });
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
 
-        TextView temp = (TextView) findViewById(R.id.temp);
-        temp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent tempIntent = new Intent(MainActivity.this,TempActivity.class);
-                startActivity(tempIntent);
-            }
-        });
+//        final TextView numbers  = (TextView) findViewById(R.id.numbers);
+//
+//        numbers.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+//                startActivity(numbersIntent);
+//            }
+
+//        });
+
+
+
+//        TextView family = (TextView) findViewById(R.id.family);
+//        family.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent familyIntent = new Intent(MainActivity.this,FamilyActivity.class);
+//                startActivity(familyIntent);
+//            }
+//        });
+//
+//        // Find the View that shows the colors category
+//        TextView colors = (TextView) findViewById(R.id.colors);
+//
+//        // Set a click listener on that View
+//        colors.setOnClickListener(new View.OnClickListener() {
+//            // The code in this method will be executed when the colors category is clicked on.
+//            @Override
+//            public void onClick(View view) {
+//                // Create a new intent to open the {@link ColorsActivity}
+//                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+//
+//                // Start the new activity
+//                startActivity(colorsIntent);
+//            }
+//        });
+//
+//        // Find the View that shows the phrases category
+//        TextView phrases = (TextView) findViewById(R.id.phrases);
+//
+//        // Set a click listener on that View
+//        phrases.setOnClickListener(new View.OnClickListener() {
+//            // The code in this method will be executed when the phrases category is clicked on.
+//            @Override
+//            public void onClick(View view) {
+//                // Create a new intent to open the {@link PhrasesActivity}
+//                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+//
+//                // Start the new activity
+//                startActivity(phrasesIntent);
+//            }
+//        });
+//
+//
+//        TextView temp = (TextView) findViewById(R.id.temp);
+//        temp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent tempIntent = new Intent(MainActivity.this,TempActivity.class);
+//                startActivity(tempIntent);
+//            }
+//        });
     }
 
 
